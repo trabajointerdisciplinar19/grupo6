4f44b86d4d809392824168b2017e03d91a74b370
LuisVillanuevaFlores <furiadorada@hotmail.com>
2019-10-24 00:21:01 -0500

    Actualización con movimiento
    Se agrego mas animaiones y movimiento al personaje

---
+++ MathPath/Assets/ChekGround.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChekGround : MonoBehaviour
+
+    
+{
+    private playercontroller player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<playercontroller>();
+
+        
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        player.grounded = true;
+    }
+    void OnCollisionExit2D(Collision2D col)
+    {
+        player.grounded = false;
+    }
+}
---
+++ MathPath/Assets/playercontroller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float maxSpeed = 5f;
+    public float speed = 2f;
+    public bool grounded;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+        anim.SetBool("Grounded", grounded);
+        
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        rb2d.AddForce(Vector2.right * speed * h);
+
+
+
+        float limiteSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+            rb2d.velocity = new Vector2(limiteSpeed, rb2d.velocity.y);
+        
+        Debug.Log(rb2d.velocity.x);
+    }
+}
