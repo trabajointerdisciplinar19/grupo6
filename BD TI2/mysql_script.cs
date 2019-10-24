3f0dc4efb3019dc2a98f8d13033bc3da92e6b9af
renzovc987 <38012028+renzovc987@users.noreply.github.com>
2019-10-24 08:49:38 -0500

    Add files via upload

---
+++ BD TI2/mysql_script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+public class mysql_script : MonoBehaviour {
+
+	void Start () {
+        Mysql();
+	}
+
+	void Mysql()
+    {
+        MySqlConnection conn = new MySqlConnection();
+        string DataConecction = "Server=localhost;Database=tutorial;Uid=root;Pwd=1234;";
+
+        conn.ConnectionString = DataConecction;
+
+        try
+        {
+            conn.Open();
+            Debug.Log("Conexión establecida");
+        }
+        catch(MySqlException error)
+        {
+            Debug.Log("Conexión fallida: " + error);
+        }
+    }
+}
