b79243f1d860a0d352c1fa397012792ab0965c02
JesusTacca <job1and2ramza@gmail.com>
2019-10-25 16:25:12 -0500

    Dino

---
+++ MathPath/Assets/Dinosaurio_Walk.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dinosaurio_Walk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D dinosaurio;
+    public float maxvelocidad;
+    bool voltearDinosaurio=true;
+    SpriteRenderer DinoRenderer;
+    void Start()
+    {
+        dinosaurio = GetComponent<Rigidbody2D> ();
+        DinoRenderer = GetComponent<SpriteRenderer> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mover = Input.GetAxis("Horizontal");
+        if(mover>0 && !voltearDinosaurio)
+        {
+            Voltear();
+        }else if(mover<0 && voltearDinosaurio)
+        {
+            Voltear();
+        }
+        dinosaurio.velocity = new Vector2(mover * maxvelocidad, dinosaurio.velocity.y);
+    }
+    void Voltear(){
+        voltearDinosaurio = !voltearDinosaurio;
+        DinoRenderer.flipX = !DinoRenderer.flipX;
+    }
+}
