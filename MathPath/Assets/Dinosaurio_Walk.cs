04e6490b55ec5924716f2a5790dda5af41f6d72a
JesusTacca <job1and2ramza@gmail.com>
2019-10-25 23:44:37 -0500

    Salto y Escenario

--- MathPath/Assets/Dinosaurio_Walk.cs
+++ MathPath/Assets/Dinosaurio_Walk.cs
@@ -7,9 +7,18 @@ public class Dinosaurio_Walk : MonoBehaviour
     // Start is called before the first frame update
     Rigidbody2D dinosaurio;
     public float maxvelocidad;
-    bool voltearDinosaurio=true;
-    SpriteRenderer DinoRenderer;
     Animator DinosaurioAnim;
+    bool puedeMover = true;
+
+    //saltar
+    bool EnSuelo = false;
+    float CheakRadioSuelo = 0.2f;
+    public LayerMask capaSuelo;
+    public Transform chequearSuelo;
+    public float poderSalto;
+
+    bool voltearDinosaurio = true;
+    SpriteRenderer DinoRenderer;
     void Start()
     {
         dinosaurio = GetComponent<Rigidbody2D> ();
@@ -20,19 +29,40 @@ public class Dinosaurio_Walk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mover = Input.GetAxis("Horizontal");
-        if(mover>0 && !voltearDinosaurio)
+        if(puedeMover && EnSuelo && Input.GetAxis("Jump") > 0)
         {
-            Voltear();
-        }else if(mover<0 && voltearDinosaurio)
-        {
-            Voltear();
+            DinosaurioAnim.SetBool("EstaSuelo", false);
+            dinosaurio.velocity = new Vector2(dinosaurio.velocity.x, 0f);
+            dinosaurio.AddForce(new Vector2(0, poderSalto), ForceMode2D.Impulse);
+            EnSuelo = false;
         }
-        dinosaurio.velocity = new Vector2(mover * maxvelocidad, dinosaurio.velocity.y);
-        DinosaurioAnim.SetFloat("Velocidad_movimiento", Mathf.Abs(mover));
+        EnSuelo = Physics2D.OverlapCircle(chequearSuelo.position, CheakRadioSuelo, capaSuelo);
+        DinosaurioAnim.SetBool("EstaSuelo", EnSuelo);
+        float mover = Input.GetAxis("Horizontal");
+        if (puedeMover)
+        {
+            if (mover > 0 && !voltearDinosaurio)
+            {
+                Voltear();
+            }
+            else if (mover < 0 && voltearDinosaurio)
+            {
+                Voltear();
+            }
+            dinosaurio.velocity = new Vector2(mover * maxvelocidad, dinosaurio.velocity.y);
+            DinosaurioAnim.SetFloat("Velocidad_movimiento", Mathf.Abs(mover));
+        } else
+        {
+            dinosaurio.velocity = new Vector2(0, dinosaurio.velocity.y);
+            DinosaurioAnim.SetFloat("Velocidad_movimiento", 0);
+        }
     }
     void Voltear(){
         voltearDinosaurio = !voltearDinosaurio;
         DinoRenderer.flipX = !DinoRenderer.flipX;
+    }
+    public void togglePuedeMover()
+    {
+        puedeMover = !puedeMover;
     }
 }
