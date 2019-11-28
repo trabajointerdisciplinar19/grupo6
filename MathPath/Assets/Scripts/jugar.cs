13f25b87deb6c083d9bfad7e91341579a77bc2d3
LuisVillanuevaFlores <furiadorada@hotmail.com>
2019-11-28 00:23:39 -0500

    BOTON AYUDA, ELEGIR PERSONAJE Y MEJORAS DE MENU

---
+++ MathPath/Assets/Scripts/jugar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jugar : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.LoadLevel("SampleScene");
+            
+        }
+    }
+}
